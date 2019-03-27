2d4cd012c9787e8198379a3a5a8bc70964a55e4c
Kayoku <jordan.bouchoucha@protonmail.com>
2019-03-27 14:46:48 +0100

    omegawatt script + snmpget basic usage

---
+++ omegawatt.py
@@ -0,0 +1,48 @@
+"""
+Specific module for Lyon/Grenoble G5K wattmetter (OmegaWatt)
+Source from: https://gitlab.inria.fr/delamare/wattmetre-read/raw/master/tools/getwatt.py
+"""
+
+def getwatt(node, from_ts, to_ts):
+    """
+    Get power values from Grid'5000 Lyon Wattmetre (requires Execo)
+
+    :param node: Node name
+
+    :param from_ts: Time from which metric is collected, as an integer Unix timestamp
+
+    :param from_ts: Time until which metric is collected, as an integer Unix timestamp
+
+    :return: A list of (timestamp, value) tuples.
+    """
+
+    import datetime
+    import requests
+    import gzip
+    import time
+    from execo_g5k import get_host_attributes
+
+    watt = []
+    node_wattmetre = get_host_attributes(node)['sensors']['power']['via']['pdu'][0]
+    for ts in range(int(from_ts), int(to_ts)+3600, 3600):
+        suffix = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H')
+        if suffix != datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H'):
+            suffix += ".gz"
+        data=requests.get("http://wattmetre.lyon.grid5000.fr/data/"+node_wattmetre['uid']+"-log/power.csv."+suffix).content
+        if suffix.endswith(".gz"):
+            data = gzip.decompress(data)
+        for l in str(data).split('\\n')[1:-1]:
+            l = l.split(',')
+            if l[3] == 'OK' and l[4+node_wattmetre['port']] != '':
+                ts, value = (float(l[2]), float(l[4+node_wattmetre['port']]))
+                if from_ts <= ts and ts <= to_ts:
+                    watt.append((ts, value))
+        if not suffix.endswith(".gz"):
+            break
+    return watt
+
+
+# node = "orion-4"
+# from_ts = 1553595857
+# to_ts =   1553596857
+# print(getwatt(node, from_ts, to_ts))
---
+++ snmpget.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+val=0
+cmd1="snmpget -Oqv -v2c -c public pdu-b3p1.lille.grid5000.fr iso.3.6.1.4.1.318.1.1.26.9.4.3.1.7.10"
+cmd2="snmpget -Oqv -v2c -c public pdu-b3p2-1.lille.grid5000.fr iso.3.6.1.4.1.318.1.1.26.9.4.3.1.7.10"
+
+while [ 1 ]
+do
+	req1=`eval $cmd1`
+	req2=`eval $cmd2`
+	res=$(($req1 + $req2))
+	if [ $val -ne $res ]
+	then
+		echo $res
+		val=$res
+	fi
+done
